48f085bbb39add6e84509f19cd746b24e37118f2
Davi Luquini <davi262016@gmail.com>
2025-06-11 10:44:22 -0300

    new lesson

--- Seção 5 Binding, Diretivas, Templates e Decorators/Decorators/src/app/card/card.component.ts
+++ Seção 5 Binding, Diretivas, Templates e Decorators/Decorators/src/app/card/card.component.ts
@@ -6,8 +6,17 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  @Input({ required: true }) planType: string = '';
   @Input({ required: true, alias: 'planPrice' }) planPrice: number = 0;
+
+  private _planType: string = '';
+
+  @Input({ required: true }) set planType(value: string) {
+    this._planType = value.toUpperCase();
+  }
+
+  get planType(): string {
+    return this._planType;
+  }
 
   buttonClicked(valueEmitted: boolean) {
     console.log('buttonClicked ' + valueEmitted);
